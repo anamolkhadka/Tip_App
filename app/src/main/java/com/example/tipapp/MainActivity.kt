a6ea77204cb13ac7cdc50df6a39251183bed9323
iamAnamol <khadkaanamol8@gmail.com>
2022-11-08 04:21:21 -0600

    Adding files

---
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.tipapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tipapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        if(cost == null) {
+            binding.tipResult.text = ""
+            binding.totalResult.text = ""
+            return
+        }
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        var total = cost + tip
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
+        binding.totalResult.text = getString(R.string.total_amount,formattedTotal)
+
+
+    }
+}
